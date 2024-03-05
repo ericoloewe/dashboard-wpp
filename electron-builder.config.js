7f83134d5260bad11a9a258400024216d38ff0d5
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-03-05 12:21:08 -0300

    env

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,61 +1,132 @@
-var builder = require("electron-builder")
-var path = require("path")
-var Platform = builder.Platform
+const builder = require("electron-builder")
+const { GitHubPublisher } = require("electron-publish/out/gitHubPublisher")
+const { CancellationToken } = require("builder-util-runtime/out/CancellationToken")
+const Platform = builder.Platform
+const path = require('path');
+const { readdir } = require('fs');
 
-console.log("HEREE");
+require('dotenv').config()
 
-// Let's get that intellisense working
-/**
-* @type {import('electron-builder').Configuration}
-* @see https://www.electron.build/configuration/configuration
-*/
-var options = {
-  extends: null,
-  appId: "com.loewe.dashbord",
-  // "store" | “normal" | "maximum". - For testing builds, use 'store' to reduce build time significantly.
-  compression: "store",
-  removePackageScripts: true,
-  win: { target: 'nsis' },
-  nsis: { deleteAppDataOnUninstall: true },
-  mac: null,
-  target: [
-    { target: 'win' },
-  ],
-  asar: false,
-  files: [
-    "build/**/*",
-    "package.json",
-    "!**/node_modules/*/{CHANGELOG.md,README.md,README,readme.md,readme}",
-    "!**/node_modules/*/{test,__tests__,tests,powered-test,example,examples}",
-    "!**/node_modules/*.d.ts",
-    "!**/node_modules/.bin",
-    "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj}",
-    "!.editorconfig",
-    "!**/._*",
-    "!**/{.DS_Store,.git,.hg,.svn,CVS,RCS,SCCS,.gitignore,.gitattributes}",
-    "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.nyc_output}",
-    "!**/{appveyor.yml,.travis.yml,circle.yml}",
-    "!**/{npm-debug.log,yarn.lock,.yarn-integrity,.yarn-metadata.json}",
-    // "!node_modules",
-    // "node_modules/whatsapp-web.js/**/*",
-    // "node_modules/electron/**/*",
-    // "node_modules/puppeteer/**/*",
-    // "node_modules/debug/**/*",
-    // "node_modules/ms/**/*",
-    // "node_modules/@electron/**/*",
-  ],
-  includeSubNodeModules: false,
-  extraMetadata: {
-    main: "build/electron/starter.js"
+
+function run() {
+  if (process.argv.includes('--publish')) {
+    publish();
+  } else {
+    // Promise is returned
+    build();
   }
-};
+}
 
+function publish() {
+  var pjson = require('./package.json');
+  console.log(pjson.version);
 
+  /**
+   * @type {import('builder-util-runtime/out/publishOptions').GithubOptions}
+   * @see https://www.electron.build/configuration/configuration
+  */
+  const info = {
+    provider: 'github',
+    owner: 'ericoloewe',
+    repo: 'dashboard-wpp',
+    private: true,
+  };
 
-// Promise is returned
-builder.build({
-  targets: Platform.WINDOWS.createTarget(),
-  config: options
-}).then(function (result) {
-  console.log(JSON.stringify(result));
-}).catch(console.error);
+  const cancellationToken = new CancellationToken();
+
+  const publisher = new GitHubPublisher({ cancellationToken: cancellationToken }, info, pjson.version, { publish: 'always' });
+  const distPath = path.join(__dirname, './dist');
+
+  readdir(distPath, async (err, files) => {
+    const releaseFilesPromises = files
+      .filter(x => x.includes('dashboard-gestao'))
+      .map(x => path.join(distPath, x))
+      .map(x => publishFile(publisher, x));
+
+    Promise.all(releaseFilesPromises)
+      .then(function (result) {
+        console.log(JSON.stringify(result));
+      }).catch(console.error);
+  });
+}
+
+function build() {
+  /**
+    * @type {import('electron-builder').Configuration}
+    * @see https://www.electron.build/configuration/configuration
+    */
+  const options = {
+    extends: null,
+    appId: "com.loewe.dashbord",
+    // "store" | “normal" | "maximum". - For testing builds, use 'store' to reduce build time significantly.
+    compression: "store",
+    removePackageScripts: true,
+    win: {
+      target: 'nsis',
+      publish: ['github']
+    },
+    publish: {
+      provider: 'github',
+      owner: 'ericoloewe',
+      repo: 'dashboard-wpp',
+      private: true,
+    },
+    nsis: { deleteAppDataOnUninstall: true, oneClick: false },
+    mac: null,
+    target: [
+      { target: 'win' },
+    ],
+    asar: false,
+    files: [
+      "build/**/*",
+      "package.json",
+      "!**/node_modules/*/{CHANGELOG.md,README.md,README,readme.md,readme}",
+      "!**/node_modules/*/{test,__tests__,tests,powered-test,example,examples}",
+      "!**/node_modules/*.d.ts",
+      "!**/node_modules/.bin",
+      "!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj}",
+      "!.editorconfig",
+      "!**/._*",
+      "!**/{.DS_Store,.git,.hg,.svn,CVS,RCS,SCCS,.gitignore,.gitattributes}",
+      "!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.nyc_output}",
+      "!**/{appveyor.yml,.travis.yml,circle.yml}",
+      "!**/{npm-debug.log,yarn.lock,.yarn-integrity,.yarn-metadata.json}",
+      // "!node_modules",
+      // "node_modules/whatsapp-web.js/**/*",
+      // "node_modules/electron/**/*",
+      // "node_modules/puppeteer/**/*",
+      // "node_modules/debug/**/*",
+      // "node_modules/ms/**/*",
+      // "node_modules/@electron/**/*",
+    ],
+    includeSubNodeModules: false,
+    extraMetadata: {
+      main: "build/electron/starter.js"
+    }
+  };
+
+  builder.build({
+    targets: Platform.WINDOWS.createTarget(),
+    config: options
+  }).then(function (result) {
+    console.log(JSON.stringify(result));
+  }).catch(console.error);
+}
+
+/**
+ * 
+ * @param {import('electron-publish/out/gitHubPublisher').GitHubPublisher} publisher 
+ * @param {string} filePath 
+ */
+function publishFile(publisher, filePath) {
+  /**
+  * @type {import('electron-publish').UploadTask}
+  */
+  const task = {
+    file: filePath,
+  };
+
+  return publisher.upload(task)
+}
+
+run();
---
+++ public/electron/ipc.js
@@ -0,0 +1,114 @@
+const { BrowserWindow, ipcMain } = require('electron');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+/**
+ * @type {import('whatsapp-web.js').Client}
+ */
+let client = null;
+
+ipcMain.on('init-login', (event) => {
+  console.log("init login");
+
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+
+  if (client !== null)
+    return runReady(win, client);
+
+  client = new Client({ authStrategy: new LocalAuth() });
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    win.webContents.send('qrcode-loaded', qr);
+  });
+
+  client.on('ready', () => runReady(win, client));
+
+  client.initialize();
+});
+
+ipcMain.on('load-chats', async (event, groupId) => {
+  console.log('Load group chats!');
+
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  const response = await client.getChats();
+  const groups = response.filter(x => x.isGroup).slice(0, 6);
+  const profilesPromises = groups.map(x => client.getProfilePicUrl(x.id._serialized));
+
+  const pictures = await Promise.all(profilesPromises);
+
+  pictures.forEach((x, i) => {
+    groups[i].profilePicture = x;
+  });
+
+  for (let index = 0; index < groups.length; index++) {
+    const contact = groups[index];
+    const participants = contact.groupMetadata.participants.slice(0, 5);
+
+    var participantsPictures = await Promise.all(participants.map(x => client.getProfilePicUrl(x.id._serialized)));
+
+    participantsPictures.forEach((x, i) => {
+      participants[i].profilePicture = x;
+    });
+  }
+
+  win.webContents.send('chats-loaded', groups);
+});
+
+ipcMain.on('load-group-info', async (event, groupId) => {
+  console.log('Load group info!');
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+
+  const chat = await client.getChatById(groupId);
+  const participants = chat?.groupMetadata?.participants;
+
+  const contactsPromises = participants?.map(x => client.getContactById(x.id._serialized));
+  const contacts = await Promise.all(contactsPromises);
+
+  contacts.forEach((x, i) => {
+    participants[i].contact = x;
+  });
+
+  const profilesPromises = participants?.map(x => client.getProfilePicUrl(x.id._serialized));
+  const pictures = await Promise.all(profilesPromises);
+
+  pictures.forEach((x, i) => {
+    participants[i].profilePicture = x;
+  });
+
+  win.webContents.send('group-info-loaded', chat);
+});
+ipcMain.on('load-group-messages', async (event, groupId) => {
+  console.log('Load group messages!');
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  const chat = await client.getChatById(groupId);
+  const messages = await chat.fetchMessages({ limit: 50 });
+
+  // console.log(messages);
+  win.webContents.send('group-messages-loaded', messages);
+});
+ipcMain.on('load-participant-info', async (event, participantId) => {
+  console.log('Load group messages!');
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  const contact = await client.getContactById(participantId);
+  // console.log(messages);
+  win.webContents.send('participant-info-loaded', contact);
+});
+ipcMain.on('load-participant-messages', async (event, { groupId, participantId }) => {
+  console.log('Load group messages!');
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  const chat = await client.getChatById(groupId);
+  const messages = await chat.fetchMessages({ limit: 1000 });
+  // console.log(messages);
+  win.webContents.send('participant-messages-loaded', messages.filter(x => x.author === participantId));
+});
+
+async function runReady(win) {
+  console.log('Client is ready!');
+  win.webContents.send('whats-ready', 'ok');
+}
