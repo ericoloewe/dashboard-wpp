f394c03bd6a6cd7c00ca763e40e651299621df39
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-03-08 10:29:07 -0300

    login

--- src/login.js
+++ src/login.js
@@ -4,14 +4,24 @@ export function Login() {
   const { doLogin, qrcode, isLoadingAuth } = useAuth()
 
   return <header className="App-header">
-    <img src={qrcode || './logo.png'} className={`App-logo qrcode-ok`} alt="logo" />
+    {qrcode
+      ? (
+        <>
+          <img src={qrcode} className={`App-logo qrcode-ok`} alt="logo" />
+          <h3>Para entrar no dashboard, leia o QR CODE através do seu WhatsApp</h3>
+        </>
+      )
+      : (
+        <>
+          <img src={'./logo.png'} className={`App-logo qrcode-ok`} alt="logo" />
+          <h1>Dashboard de Gestão do WhatsApp</h1>
 
-    {!qrcode && (
-      <button type='button' className='btn btn-primary' onClick={e => doLogin()} disabled={isLoadingAuth}>
-        {isLoadingAuth
-          ? <><span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...</>
-          : <>Entrar / Gerar Qr Code</>}
-      </button>
-    )}
+          <button type='button' className='btn btn-primary' onClick={e => doLogin()} disabled={isLoadingAuth}>
+            {isLoadingAuth
+              ? <><span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...</>
+              : <>Entrar / Gerar Qr Code</>}
+          </button>
+        </>
+      )}
   </header>
 }
--- src/participants.js
+++ src/participants.js
@@ -3,6 +3,7 @@ import { Layout } from './layout';
 import { useNavigate, useParams } from "react-router-dom";
 import { Loader } from './loader';
 import moment from 'moment';
+import { Modal } from './modal';
 
 export function Participants() {
   const { groupId, participantId } = useParams();
@@ -136,40 +137,3 @@ function Messages({ messages, info, onMediaClick }) {
   </ul>;
 }
 
-function Modal({ children, title, onClose }) {
-  const [isOpen, setIsOpen] = useState(true);
-
-  function onCloseClick() {
-    setIsOpen(false)
-    onClose();
-  }
-
-  function onCloseClickBackdrop(event) {
-    if (event?.target?.id === 'modal') {
-      onCloseClick();
-    }
-  }
-
-
-  return (
-    <>
-      <div className={`modal modal-lg fade ${isOpen && 'show'}`} id="modal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{ zIndex: 99, display: isOpen && 'block' }} onClick={onCloseClickBackdrop}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onCloseClick}></button>
-            </div>
-            <div className="modal-body">
-              {children}
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={onCloseClick}>Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={`modal-backdrop fade ${isOpen && 'show'}`} style={{ zIndex: 98 }} onClick={onCloseClickBackdrop}></div>
-    </>
-  )
-}
